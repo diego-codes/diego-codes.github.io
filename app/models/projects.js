e1a26407d96220ca9cd68eaa0d6145f13037ea54
Diego Hernandez <diego@cuyaproductions.com>
2016-10-30 17:55:10 -0500

    Add project photos and tools

--- app/models/projects.js
+++ app/models/projects.js
@@ -1,6 +1,61 @@
 const db = require('../helpers/db');
+const Promise = require('promise');
 
-const getAll = (callback) => {
+function getProjectTools(projectId) {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT name FROM tools WHERE id IN (
+        SELECT tool_id FROM project_tools WHERE project_id=${projectId}
+      )`, (queryError, tools) => {
+        if (queryError) {
+          reject(queryError);
+        }
+
+        const toolNames = [];
+        for (const tool of tools.rows) {
+          toolNames.push(tool.name);
+        }
+        resolve(toolNames);
+    });
+  });
+}
+
+function getProjectRoles(projectId) {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT name FROM roles WHERE id IN (
+        SELECT role_id FROM project_roles WHERE project_id=${projectId}
+      )`, (queryError, roles) => {
+        if (queryError) {
+          reject(queryError);
+        }
+
+        const roleNames = [];
+        for (const role of roles.rows) {
+          roleNames.push(role.name);
+        }
+        resolve(roleNames);
+    });
+  });
+}
+
+function getProjectImages(projectId) {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT url, description
+      FROM project_photos
+      WHERE project_id=${projectId}`, (queryError, images) => {
+        if (queryError) {
+          reject(queryError);
+        }
+
+        const imagesData = [];
+        for (const image of images.rows) {
+          imagesData.push(image);
+        }
+        resolve(imagesData);
+    });
+  });
+}
+
+function getAll(callback) {
   db.query('SELECT id, title FROM projects ORDER BY title', (queryError, result) => {
     if (queryError) {
       return callback(queryError);
@@ -9,21 +64,12 @@ const getAll = (callback) => {
   });
 }
 
-const getQuery = `
-  SELECT title, description, github_link, demo_link
-  FROM projects
-  WHERE id`;
-
-const getRolesQuery = `
-  SELECT roles.name
-  FROM roles, projects, project_roles
-  WHERE project_roles.project_id=projects.id
-  AND project_roles.role_id=roles.id
-  AND projects.id=`;
-
-const get = (projectId, callback) => {
+function get(projectId, callback) {
   projectId = parseInt(projectId);
-  db.query(`${getQuery} BETWEEN ${projectId} AND ${projectId + 1}`, (queryError, data) => {
+
+  db.query(`SELECT title, description, github_link, demo_link
+  FROM projects
+  WHERE id BETWEEN ${projectId} AND ${projectId + 1}`, (queryError, data) => {
     if (queryError) {
       return callback(queryError);
     }
@@ -32,14 +78,16 @@ const get = (projectId, callback) => {
     if (data.rows[1]) {
       result.next = projectId + 1;
     }
-
-    db.query(`${getRolesQuery}${projectId}`, (queryError, roles) => {
-      result.roles = [];
-      for (const role of roles.rows) {
-        result.roles.push(role.name);
-      }
-      return callback(null, result);
-    });
+    Promise.all([getProjectRoles(projectId), getProjectTools(projectId), getProjectImages(projectId)])
+      .then((data) => {
+        result.roles = data[0];
+        result.tools = data[1];
+        result.images = data[2];
+        callback(null, result);
+      })
+      .catch((error) => {
+        callback(error);
+      });
   });
 }
 
